01b50bf7425c82d9389452632d9bf63abea5d2a8
mdahmen <manuel.dahmen@gmail.com>
2015-03-21 16:06:25 +0100

    Enfin des B-Splines d'ordre N :+1:  :accept:

---
+++ src/main/java/courbes_bsplines/TestGDXBSpline.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package courbes_bsplines;
+
+import info.emptycanvas.library.gdximports.gdx_BSplineCurve;
+import info.emptycanvas.library.object.ColorTexture;
+import info.emptycanvas.library.object.Point3D;
+import info.emptycanvas.library.testing.TestObjet;
+import static info.emptycanvas.library.testing.TestObjet.GENERATE_IMAGE;
+import static info.emptycanvas.library.testing.TestObjet.GENERATE_MOVIE;
+import java.awt.Color;
+
+/**
+ *
+ * @author Manuel Dahmen <manuel.dahmen@gmail.com>
+ */
+public class TestGDXBSpline extends TestObjet
+{
+    private gdx_BSplineCurve b;
+ @Override
+    public void finit() {
+        scene().clear();
+
+        b = new gdx_BSplineCurve();
+
+        
+        b.instantiate(TestsBSpline.p(frame()), 3);
+
+        b.texture(new ColorTexture(Color.WHITE));
+
+        scene().add(b);
+
+        scene.cameraActive().setCamera(Point3D.Z.mult(-(2 * frame() + 2)));
+
+    }
+
+    public static void main(String[] args) {
+        TestGDXBSpline ts = new TestGDXBSpline();
+
+        ts.setGenerate(GENERATE_IMAGE | GENERATE_MOVIE);
+
+        ts.loop(false);
+
+        ts.setMaxFrames(10);
+
+        new Thread(ts).start();
+
+    }
+
+    @Override
+    public void afterRenderFrame() {
+    }
+
+    @Override
+    public void ginit() {
+    }
+
+    @Override
+    public void testScene() throws Exception {
+    }
+
+    public void afterRender() {
+    }
+}
