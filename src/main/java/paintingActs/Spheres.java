ed2522067d66b02b6bf3babbdcf1a233bcd50ecc
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-10-12 14:20:11 +0200

    Correct address

---
+++ src/main/java/paintingActs/Spheres.java
@@ -0,0 +1,14 @@
+package paintingActs;
+
+import info.emptycanvas.library.testing.TestObjetStub;
+
+/**
+ * Created by manue on 12-10-15.
+ */
+public class Spheres extends TestObjetStub {
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/paintingActs/SpheresPA.java
@@ -0,0 +1,14 @@
+package paintingActs;
+
+import info.emptycanvas.library.object.PaintingAct;
+
+/**
+ * Created by manue on 12-10-15.
+ */
+public class SpheresPA extends PaintingAct {
+
+    @Override
+    public void paint() {
+
+    }
+}
