1cfc8a56109f0535daa1b1578aa2281b3d475618
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-10-24 09:50:27 +0200

    Test PaintingAct: OK

--- src/main/java/paintingActs/SpheresPA.java
+++ src/main/java/paintingActs/SpheresPA.java
@@ -1,14 +1,42 @@
 package paintingActs;
 
+import info.emptycanvas.library.object.ColorTexture;
 import info.emptycanvas.library.object.PaintingAct;
+import info.emptycanvas.library.tribase.TRISphere;
+
+import java.awt.*;
 
 /**
  * Created by manue on 12-10-15.
  */
 public class SpheresPA extends PaintingAct {
+    double s[][];
+
+    public void ginit() {
+        s = new double[][]{{0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}};
+    }
+
+    public void finit() {
+        double pas = 0.001;
+        for (int i = 0; i < s[0].length; i++) {
+            s[0][i] += (Math.random() - 0.5) * Math.PI * 2 / pas;
+            s[1][i] += (Math.random() - 0.5) * Math.PI * 2 / pas;
+        }
+    }
 
     @Override
     public void paint() {
+        if (s == null)
+            ginit();
+        finit();
+        if (getObjet() instanceof TRISphere) {
+            TRISphere sph = (TRISphere) getObjet();
 
+            for (int i = 0; i < s[0].length; i++) {
+                TRISphere s2 = new TRISphere(sph.calculerPoint3D(s[0][i], s[1][i]), 2);
+                s2.texture(new ColorTexture(Color.BLUE));
+                getZBuffer().dessinerSilhouette3D(s2);
+            }
+        }
     }
 }
---
+++ src/main/java/paintingActs/TestSpheres.java
@@ -0,0 +1,36 @@
+package paintingActs;
+
+import info.emptycanvas.library.object.ColorTexture;
+import info.emptycanvas.library.object.Point3D;
+import info.emptycanvas.library.testing.TestObjet;
+import info.emptycanvas.library.testing.TestObjetStub;
+import info.emptycanvas.library.tribase.TRISphere;
+
+import java.awt.*;
+
+/**
+ * Created by manue on 12-10-15.
+ */
+public class TestSpheres extends TestObjetStub {
+    public void ginit() {
+        TRISphere sphere = new TRISphere(Point3D.O0, 10);
+        sphere.texture(new ColorTexture(Color.GREEN));
+        scene().add(sphere);
+        sphere.setPaintingAct(getZ(), scene(), new SpheresPA());
+    }
+
+    public void testScene() {
+
+    }
+
+    public static void main(String[] args) {
+        TestObjet to = new TestSpheres();
+
+        to.loop(true);
+        to.unterminable(false);
+        to.setGenerate(GENERATE_IMAGE | GENERATE_MOVIE);
+        to.setMaxFrames(2000);
+
+        new Thread(to).start();
+    }
+}
